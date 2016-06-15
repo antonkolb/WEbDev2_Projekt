9ac51e302bd0afc537e3983c8baba9ff0c437fc5
Christian Pöhlmann <Poehc@users.noreply.github.com>
2016-06-15 11:46:45 +0200

    K :

---
+++ common/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+ 'class' => 'yii\db\Connection',
+ 'dsn' => 'mysql:host=localhost;dbname=team-17',
+ 'username' => 'team-17',
+ 'password' => 'qWqqsG8VCXmN3ZaL',
+ 'charset' => 'utf8',
+];
