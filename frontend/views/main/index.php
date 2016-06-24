a679423ec5bcc1aba8c9059a424fb990823ab433
August Kraft <antonkolb@users.noreply.github.com>
2016-06-24 19:52:05 +0200

    Main (CSS, Links angepasst)

--- frontend/views/main/index.php
+++ frontend/views/main/index.php
@@ -4,6 +4,8 @@
 /* @var $this yii\web\View */
 
 $this->title = 'Rechnen und Denken';
+//eigene CSS
+$this->registerCssFile(Yii::$app->request->baseUrl.'/css/main.css');
 ?>
 <div class="site-index">
 
@@ -14,11 +16,11 @@ $this->title = 'Rechnen und Denken';
         <p class="lead">Aufgabe 2: Textaufgaben</p>
         <p class="lead">Aufgabe 3: bla</p>
         <p class="lead">Aufgabe 4: blub</p>
-		
-        <p><a class="btn btn-md btn-info gamebtn" href="aufgabe1">Aufgabe 1</a>	</p>
-        <p><a class="btn btn-md btn-info gamebtn" href="aufgabe2">Aufgabe 2</a>	</p>
-        <p><a class="btn btn-md btn-info gamebtn" href="aufgabe3">Aufgabe 3</a>	</p>
-        <p><a class="btn btn-md btn-info gamebtn" href="aufgabe4">Aufgabe 4</a>	</p>
 
+        <p><a class="btn btn-md btn-info gamebtn" href="main/aufgabe1">Aufgabe 1</a>
+	   <a class="btn btn-md btn-info gamebtn" href="main/aufgabe2">Aufgabe 2</a>
+      	   <a class="btn btn-md btn-info gamebtn" href="main/aufgabe3">Aufgabe 3</a>
+	   <a class="btn btn-md btn-info gamebtn" href="main/aufgabe4">Aufgabe 4</a>
+	</p>
     </div>
 </div>
