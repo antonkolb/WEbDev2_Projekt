8f7c9b28051300cfe41f3447c0a263921f54fcd7
August Kraft <antonkolb@users.noreply.github.com>
2016-06-24 19:30:41 +0200

    Hauptseite erstellt

--- frontend/controllers/MainController.php
+++ frontend/controllers/MainController.php
@@ -7,13 +7,54 @@ use frontend\controllers\AbstractController;
 class MainController extends AbstractController{
 
 	public function actionIndex(){
-		return 'You are on page customer -> index';
+		return $this->render('index'); 
 	}
 	public function actionAdd(){
 		return 'You are on page customer -> add new';
 	}
 
 
+    /**
+     *
+     *
+     * @return view der Aufgabe
+     */
+    public function actionAufgabe1()
+    {
+        //return $this->render('aufgabe1');
+	return "Aufgabe 1";
+    }
 
+    /**
+     *
+     *
+     * @return view der Aufgabe
+     */
+    public function actionAufgabe2()
+    {
+        //return $this->render('aufgabe2');
+	return "Aufgabe 2";
+    }
 
+    /**
+     *
+     *
+     * @return view der Aufgabe
+     */
+    public function actionAufgabe3()
+    {
+        //return $this->render('aufgabe3');
+	return "Aufgabe 3";
+    }
+
+    /**
+     *
+     *
+     * @return view der Aufgabe
+     */
+    public function actionAufgabe4()
+    {
+        //return $this->render('aufgabe4');
+	return "Aufgabe 4";
+    }
 }
---
+++ frontend/views/main/index.php
@@ -0,0 +1,24 @@
+
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Rechnen und Denken';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>W&auml;hle eine Aufgabe aus!</h1>
+
+        <p class="lead">Aufgabe 1: Einfache Rechenaufgaben</p>
+        <p class="lead">Aufgabe 2: Textaufgaben</p>
+        <p class="lead">Aufgabe 3: bla</p>
+        <p class="lead">Aufgabe 4: blub</p>
+		
+        <p><a class="btn btn-md btn-info gamebtn" href="aufgabe1">Aufgabe 1</a>	</p>
+        <p><a class="btn btn-md btn-info gamebtn" href="aufgabe2">Aufgabe 2</a>	</p>
+        <p><a class="btn btn-md btn-info gamebtn" href="aufgabe3">Aufgabe 3</a>	</p>
+        <p><a class="btn btn-md btn-info gamebtn" href="aufgabe4">Aufgabe 4</a>	</p>
+
+    </div>
+</div>
