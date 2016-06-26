57387c8f483995a28aa34cf317fd3aece20c38b2
August Kraft <antonkolb@users.noreply.github.com>
2016-06-26 18:08:16 +0200

    Gemeinsame Schnittstelle hinzugefügt

---
+++ frontend/models/main/IAdvancedGame.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models\main;
+
+/**
+ * Das IAdvancedGame Interface beschreibt eine Zukunftsorientierte 
+ * Variante des IGame Interface, daher wird von diesem geerbt, da mindestens auch
+ * die dort beschriebenen Methoden implementiert sein muessen um eine volle 
+ * Kompatibilitaet zu gewaehrleisten
+ * 
+ * Die Nutzerid soll immer mit ins Model uebertragen werden
+ * 
+ * @author David Karger
+ * @edited by Anton Kolb (comments only)
+ */
+interface IAdvancedGame extends IBasicGame {
+    
+    /**
+     * Liefert eine gerundete Prozentzahl zurueck wie weit der Nutzer
+     * im Spiel fortgeschritten ist
+     * 
+     */
+    function getProgress();
+    
+    /**
+     * Registriet einen gemachten Versuch
+     */
+    function addTry();
+    
+    /**
+     * Gibt die Anzahl der Versuche zurueck, die mit addTry rgistriert wurden
+     */
+    function getTries();
+
+    /**
+     * Setzt den Zahlenraum in dem gerechnet werden soll,
+     * zum Beispiel kann im Menue die Schwierigkeit hochgezaehlt werden.
+     * 
+     * @param type $minimumNumber
+     * @param type $maximumNumber
+     */
+    function setNumberRange($minimumNumber, $maximumNumber);
+    
+    
+    /**
+     * Speichert den aktuellen Fortschritt des Spieler, z.B in welchem Level mit
+     * wie viel Prozent richtigen Antworten
+     */
+    function saveCurrentState();
+    
+    
+    
+}
+
---
+++ frontend/models/main/IBasicGame.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\models\main;
+
+/**
+ * Das IGame Interface beschreibt das Interface, das zum implementieren 
+ * der Studienarbeit verwendet wird, um ein einheitliches Aufrufen aller
+ * Spiele zu gewäleisten
+ * 
+ * Spiele werden benannt nach dem Schema: Game<Team-Id>-<Interne Nummer>
+ * zb Game21-1 fuer erstes Spiel Gruppe 21
+ * 
+ * 
+ * @author David Karger
+ * @edited by Anton Kolb (comments only)
+ */
+
+interface IBasicGame {
+    /**
+     * Die initGame Methode kuemmert sich um die Vorbereitung des Spieles,
+     * zum Beispiel Vorbelegung der Werte
+     */
+    function initGame($level = 1, $numEx = 1);
+    
+    /**
+     * Die verifyAnswers Methode muss die vom Nutzer eingegebenen 
+     * Werte ueberpruefen
+     * @return Gibt Fehlerstring zurueck wenn Antworten inkorrekt, ansonsten leeren String
+     */
+    function verifyAnswers();
+}
+
