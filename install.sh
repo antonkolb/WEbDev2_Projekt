51801d9f3387de3c95f28294f58108aa57d8a08f
August Kraft <antonkolb@users.noreply.github.com>
2016-06-27 14:39:32 +0200

    K install.sh update

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#create folder if they don't exists
+if [ ! -d "frontend/web/assets" ]; then
+	mkdir frontend/web/assets
+fi
+
+if [ ! -d "frontend/runtime" ]; then
+	mkdir frontend/runtime
+fi
+
 #change rights for those both folders, server needs write acces to them!
 chmod 777 frontend/web/assets
 chmod 777 frontend/runtime
