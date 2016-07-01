319224b15b1494b35119a59c65eecfdecd3458f2
Christian Pöhlmann <Poehc@users.noreply.github.com>
2016-07-01 18:53:48 +0200

    migration Statistic Table

---
+++ console/migrations/m160630_115547_init_statistic_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+class m160630_115547_init_statistic_table extends Migration
+{
+    public function up()
+    {
+	$this->createTable(
+		'statistic',
+		[ 	'userid' => 'pk',
+			'username'=> 'string' -> notNull(),
+			'game'=> 'string',
+			'userAnswer' => 'string',
+			'amountOfTries' => 'time',
+			'elapsedTime' => 'time',
+			 
+		]
+	);
+    }
+
+    public function down()
+    {
+	$this->dropTable('statistic');
+        echo "m160630_115547_init_statistic_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
