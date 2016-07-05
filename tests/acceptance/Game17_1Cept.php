dbbeab30f6ea8c21d383152a3d48bce3d146f574
August Kraft <antonkolb@users.noreply.github.com>
2016-07-05 15:02:53 +0200

    Codeception Bollocks

--- tests/acceptance/Game17_1Cept.php
+++ tests/acceptance/Game17_1Cept.php
@@ -1,6 +1,7 @@
 <?php 
 $I = new AcceptanceTester($scenario);
-$I->mGoingTo('to login and select game 1 on the main page');
+$I->am('user');
+$I->amGoingTo('to login and select game 1 on the main page');
 $I->amOnPage('/');
 $username = 'Frodo Beutlin';
 $I->fillField('LoginForm[username]', $username);
@@ -13,7 +14,10 @@ $I->expect('to be in game 1');
 $I->see('Einfache Rechenaufgaben');
 
 //eigentlicher test
-$I->wantTo('submit submit no answers');
-$I->fillField('SignupForm[username]', 'test');
-$I->click('check');
+$I->wantTo('submit no answers');
+$I->click('Korrigieren');
 $I->see('User Answer cannot be blank.');
+
+
+
+//$I->fillField('Game17_1[userAnswer][1]', 'test');
---
+++ tests/acceptance/Game17_2Cept.php
@@ -0,0 +1,4 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->am('user');
+$I->wantTo('perform actions and see result');
